b163cfa7333fbeb48960894e31a3bf671dee9933
tiago-goncalves-a22201250 <148215009+tiago-goncalves-a22201250@users.noreply.github.com>
2024-12-02 17:33:47 +0000

    Inicio tecnologias e produtos

--- projeto/app/private/data/produtos/page.jsx
+++ projeto/app/private/data/produtos/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import produtos from 'produtos.json';
 
 export default function page() {
   return (
--- projeto/app/private/data/tecnologias/page.jsx
+++ projeto/app/private/data/tecnologias/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import tecnologias from '@/private/data/tecnologias.json';
 
 export default function page() {
   return (
