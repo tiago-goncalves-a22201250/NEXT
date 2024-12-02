5ef42e8fc1121b2c2586e709371c718e80eb4906
tiago-goncalves-a22201250 <148215009+tiago-goncalves-a22201250@users.noreply.github.com>
2024-12-02 15:50:49 +0000

    Inicio do editar layout

--- projeto/app/page.tsx
+++ projeto/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
-  return ( <p> Teste</p>
-  )
+import React from 'react'
+
+export default function page() {
+  return <>
+    <h1>Bem-vindo à Página de Programadores</h1>
+    <p>Esta é a página inicial da aplicação.</p>
+    </>
 }
